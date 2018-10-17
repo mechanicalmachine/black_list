ab9c83d73c60cbdb34406f3adffa36b4d231fecd
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-10-17 13:27:08 +0700

    initial model created

--- operation/models.py
+++ operation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BlackList(models.Model):
+    value = models.CharField(
+        max_length=20,
+        verbose_name='Номер телефона'
+    )
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    added_date = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        editable=False
+    )
