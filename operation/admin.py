f1ec3bb291130d33beda890d65da96f97d54dae4
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-10-18 14:54:14 +0700

    main functionality added

--- operation/admin.py
+++ operation/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from operation.models import BlackList
 
-# Register your models here.
+
+admin.site.site_header = 'Черный список телефонов'
+
+
+def add_to_black_list(modeladmin, request, queryset):
+    queryset.update(status='a')
+
+
+def exclude_from_black_list(modeladmin, request, queryset):
+    queryset.update(status='n')
+
+
+add_to_black_list.short_description = "Добавить в черный список"
+exclude_from_black_list.short_description = "Исключить из черного списка"
+
+
+@admin.register(BlackList)
+class BlackListAdmin(admin.ModelAdmin):
+    list_display = ('value', 'added_date', 'added_by', 'reason', 'status')
+    list_filter = ('added_date', )
+    ordering = ['added_date']
+    actions = [add_to_black_list, exclude_from_black_list]
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.added_by = request.user
+        super().save_model(request, obj, form, change)
--- operation/models.py
+++ operation/models.py
@@ -1,18 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+STATUS_CHOICES = (
+    ('a', 'В черном списке'),
+    ('n', 'Не в черном списке')
+)
 
 
 class BlackList(models.Model):
     value = models.CharField(
         max_length=20,
+        blank=False,
         verbose_name='Номер телефона'
-    )
-    user = models.ForeignKey(
-        "auth.User",
-        on_delete=models.CASCADE,
-        verbose_name='Пользователь'
     )
     added_date = models.DateTimeField(
         auto_now_add=True,
         blank=False,
-        editable=False
+        editable=False,
+        verbose_name='Дата добавления'
+    )
+    reason = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name='Причина добавления'
+    )
+    added_by = models.ForeignKey(
+        User,
+        null=False,
+        blank=True,
+        editable=False,
+        on_delete=models.PROTECT,
+        verbose_name='Кем добавлено'
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        verbose_name='Статус'
     )
