c761f235f7ef2caee25e90b3e8cc6f2561e4f964
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-10-18 22:59:33 +0700

    functionality for correct phones removing from blacklist added

--- operation/admin.py
+++ operation/admin.py
@@ -1,28 +1,29 @@
+import datetime
 from django.contrib import admin
-from operation.models import BlackList
+from operation.models import PhonesList
 
 
 admin.site.site_header = 'Черный список телефонов'
 
 
-def add_to_black_list(modeladmin, request, queryset):
-    queryset.update(status='a')
-
-
 def exclude_from_black_list(modeladmin, request, queryset):
-    queryset.update(status='n')
+    queryset.filter(status=True).update(
+        status=False,
+        excluded_date=datetime.datetime.now(),
+        excluded_by=request.user
+    )
 
 
-add_to_black_list.short_description = "Добавить в черный список"
 exclude_from_black_list.short_description = "Исключить из черного списка"
 
 
-@admin.register(BlackList)
-class BlackListAdmin(admin.ModelAdmin):
-    list_display = ('value', 'added_date', 'added_by', 'reason', 'status')
-    list_filter = ('added_date', )
+@admin.register(PhonesList)
+class PhonesListAdmin(admin.ModelAdmin):
+    list_display = ('value', 'added_date', 'added_by', 'reason',
+                    'status', 'excluded_date', 'excluded_by')
+    list_filter = ('status', )
     ordering = ['added_date']
-    actions = [add_to_black_list, exclude_from_black_list]
+    actions = [exclude_from_black_list]
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -31,6 +32,10 @@ class BlackListAdmin(admin.ModelAdmin):
         return False
 
     def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.added_by = request.user
-        super().save_model(request, obj, form, change)
+        if PhonesList.objects.filter(value=obj.value, status=True).first():
+            self.message_user(request, "Номер {} был добавлен ранее".format(obj.value))
+        else:
+            obj.status = True
+            if not obj.pk:
+                obj.added_by = request.user
+            super().save_model(request, obj, form, change)
--- operation/models.py
+++ operation/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 STATUS_CHOICES = (
-    ('a', 'В черном списке'),
-    ('n', 'Не в черном списке')
+    (True, 'В черном списке'),
+    (False, 'Не в черном списке')
 )
 
 
-class BlackList(models.Model):
+class PhonesList(models.Model):
     value = models.CharField(
         max_length=20,
         blank=False,
@@ -30,11 +30,27 @@ class BlackList(models.Model):
         null=False,
         blank=True,
         editable=False,
+        related_name='who_added',
         on_delete=models.PROTECT,
         verbose_name='Кем добавлено'
     )
-    status = models.CharField(
-        max_length=1,
+    status = models.BooleanField(
+        editable=False,
         choices=STATUS_CHOICES,
         verbose_name='Статус'
+    )
+    excluded_by = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        editable=False,
+        related_name='who_removed',
+        on_delete=models.PROTECT,
+        verbose_name='Кем удалено'
+    )
+    excluded_date = models.DateTimeField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name='Дата удаления'
     )
